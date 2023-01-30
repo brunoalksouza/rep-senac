f499a0a6bd15e815698ca9683bf6459b39ad6b90
Talles Silva <91147254+talles-silva@users.noreply.github.com>
2023-01-30 11:24:20 -0300

    Update styles.ts

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
-  background: #00022e;
+  background: #f1f1f1;
   height: 50px;
   align-items: center;
   justify-content: center;
