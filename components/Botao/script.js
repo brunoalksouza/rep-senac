6a878ae086a160cbd6a41d35518c8c4a1432c1c8
TatianeKishi <102524752+TatianeKishi@users.noreply.github.com>
2023-02-01 05:42:40 +0000

    adicionei um botão

---
+++ components/Botao/script.js
@@ -0,0 +1,9 @@
+const Botao = document.getElementById("btCliqueAqui")
+
+Botao.addEventListener("click", alerta)
+
+function alerta(){
+    alert("não foira atribuida nenhuma função para esse botão")
+
+    return 
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-
+import Botao from '@/components/Botao'
 
 export default function Home() {
   return (
     <>
       <Header/>
       </br>
+      <Botao/>
+      </bt>
       <Footer/>
     </>
   )
